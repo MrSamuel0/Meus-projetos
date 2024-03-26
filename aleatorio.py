8e830e0196186b4056fa0dbccfa1cfd6f4c3e4ca
Samuel Manhani Salles <151402780+MrSamuel0@users.noreply.github.com>
2024-03-25 22:26:19 -0300

    Add files via upload

---
+++ aleatorio.py
@@ -0,0 +1,33 @@
+# algorítmo 1
+"""
+
+mas = float(input('Digite a sua massa corporal (em quilogramas): '))
+alt = float(input('Digite a sua altura (metros): '))
+
+imc = mas / alt ** 2
+
+#
+
+print(f'o seu indíce de massa corporal é: {imc: .1f}')
+
+n1 = float(input('digite a primeira nota: '))
+n2 = float(input('digite a segunda nota: '))
+
+media = (n1 + n2) / 2
+
+#
+
+print(f'sua media é: {media: .1f}')
+
+bar = float(input('Tamanho da barra (em cm): '))
+
+inch = bar / 2.54
+ft = bar / 30.48
+
+print(f'o tamanho da barra em cm é: {bar: .2f}, em polegadas: {inch: .2f} e em pés: {ft: .0f}')
+
+"""
+
+mon = float(input('Insira o montante em reais'))
+
+dol = mon * 4.97
